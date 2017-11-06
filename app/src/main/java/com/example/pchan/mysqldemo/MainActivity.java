6e003516b63ace385f9222622efd1e4b3c7599dd
sarahCRichmond <sarahcrichmond@gmail.com>
2017-11-05 23:34:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pchan/mysqldemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pchan.mysqldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText UsernameEt, PasswordEt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        UsernameEt = (EditText)findViewById(R.id.etUserName);
+        PasswordEt = (EditText)findViewById(R.id.etPassword);
+    }
+
+    public void OnLogin(View view) {
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+
+        String type = "login";
+        BackgroundWorker bgworker = new BackgroundWorker(this);
+        bgworker.execute(type, username, password);
+    }
+}
