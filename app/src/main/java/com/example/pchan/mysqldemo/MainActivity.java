aa880e0844c6e7cb3f3aa21154bda861a05acfa5
sarahCRichmond <sarahcrichmond@gmail.com>
2017-11-12 18:36:23 -0600

    Started working on the DataCollector method. Added some permissions to the manifest. WIP

--- app/src/main/java/com/example/pchan/mysqldemo/MainActivity.java
+++ app/src/main/java/com/example/pchan/mysqldemo/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.pchan.mysqldemo;
 
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.provider.Settings.Secure;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     EditText UsernameEt, PasswordEt;
@@ -14,7 +20,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         UsernameEt = (EditText)findViewById(R.id.etUserName);
         PasswordEt = (EditText)findViewById(R.id.etPassword);
+
+        CollectData();
     }
+
+    public void CollectData()
+    {
+        //get dateTime
+        Date time = new Date();
+        //get device ID
+        String android_id = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
+        //get GPS location
+        LocationManager locationManager;
+        locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+
+        //test internet connection
+        boolean connected = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(this.CONNECTIVITY_SERVICE);
+        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            //we are connected to a network, so send the collected data to the DB
+            //also send any old data stored locally
+            connected = true;
+        }
+        else
+            connected = false; //we are not connected to internet, so store the collected data
+
+        Log.d("mylog","DateTime : "+time.toString());
+        Log.d("mylog","Android ID : "+android_id);
+        Log.d("mylog","GPS : "+locationManager.toString());
+        Log.d("mylog","Internet status : "+connected);
+    }
+
 
     public void OnLogin(View view) {
         String username = UsernameEt.getText().toString();
