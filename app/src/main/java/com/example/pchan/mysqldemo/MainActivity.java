fdfd8328eeb5eaf31183730493946aebb288887d
sarahCRichmond <sarahcrichmond@gmail.com>
2017-11-25 18:35:29 -0600

    Finished work on DataCollection feature.

--- app/src/main/java/com/example/pchan/mysqldemo/MainActivity.java
+++ app/src/main/java/com/example/pchan/mysqldemo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.pchan.mysqldemo;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.LocationManager;
+import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +19,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     EditText UsernameEt, PasswordEt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +38,30 @@ public class MainActivity extends AppCompatActivity {
         //get device ID
         String android_id = Secure.getString(this.getContentResolver(), Secure.ANDROID_ID);
         //get GPS location
-        LocationManager locationManager;
-        locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+        LocationManager locationManager = (LocationManager) getSystemService(this.LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+
+        // Get Current Location
+        Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        //latitude of location
+        double myLatitude = myLocation.getLatitude();
+
+        //longitude og location
+        double myLongitude = myLocation.getLongitude();
 
         //test internet connection
         boolean connected = false;
@@ -44,12 +73,18 @@ public class MainActivity extends AppCompatActivity {
             connected = true;
         }
         else
+        {
             connected = false; //we are not connected to internet, so store the collected data
+        }
 
         Log.d("mylog","DateTime : "+time.toString());
         Log.d("mylog","Android ID : "+android_id);
-        Log.d("mylog","GPS : "+locationManager.toString());
+        Log.d("mylog","GPS : " +myLatitude + myLongitude);
         Log.d("mylog","Internet status : "+connected);
+
+        String type2 = "InsertUserData";
+        DataSender dataSender = new DataSender(this);
+        dataSender.execute(type2, String.valueOf(myLongitude), String.valueOf(myLatitude), String.valueOf(android_id), time.toString(), "1" );
     }
 
 
@@ -60,5 +95,10 @@ public class MainActivity extends AppCompatActivity {
         String type = "login";
         BackgroundWorker bgworker = new BackgroundWorker(this);
         bgworker.execute(type, username, password);
+
     }
+
+
 }
+
+
