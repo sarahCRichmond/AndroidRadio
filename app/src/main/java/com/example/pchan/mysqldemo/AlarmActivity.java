b56c7fd49749ada94d5b407e8a9bd06ec6397e30
Aftershockzap <danielfranktaylor@gmail.com>
2017-11-26 21:58:06 -0600

    Added an Alarms interface and object so that saved alarms can be restored to the apps database

--- app/src/main/java/com/example/pchan/mysqldemo/AlarmActivity.java
+++ app/src/main/java/com/example/pchan/mysqldemo/AlarmActivity.java
@@ -12,12 +12,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class AlarmActivity extends AppCompatActivity {
+public class AlarmActivity extends AppCompatActivity{
     TextView curTime;
     ListView AlarmList;
     List Alist = new ArrayList();
@@ -32,9 +34,19 @@ public class AlarmActivity extends AppCompatActivity {
 
         curTime.setText(currentDateTimeString);
 
+        Date curDate = new Date();
+        SimpleDateFormat datF = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat datT = new SimpleDateFormat("hh:mm:ss");
+        String cTime = datT.format(curDate);
+        String cDate = datF.format(curDate);
+
         AlarmList = (ListView)findViewById(R.id.AlarmList);
+        Alist.add(cTime + "  " + cDate);
+        //Alist.add(cDate);
         Alist.add("Test");
         Alist.add("Test2");
+
+        testAlarm();
 
         adapter = new ArrayAdapter(AlarmActivity.this, android.R.layout.simple_list_item_1, Alist);
         AlarmList.setAdapter(adapter);
@@ -43,6 +55,25 @@ public class AlarmActivity extends AppCompatActivity {
     public void OnNewAlarm(View view) {
         Intent myIntent = new Intent(view.getContext(), SetAlarm.class);
         startActivity(myIntent);
+    }
+
+    public void testAlarm(){
+        Alarm alarm1 = new Alarm("2011-04-05", "11:55:22");
+        Alarm alarm2 = new Alarm("2011-05-05", "11:55:22");
+        Alarm alarm3 = new Alarm("2011-06-05", "11:55:22");
+        Alarm alarm4 = new Alarm("2011-07-05", "11:55:22");
+        Alarm alarm5 = new Alarm("2011-08-05", "11:55:22");
+
+        List<AlarmInt> alarmList = new ArrayList<AlarmInt>();
+        alarmList.add(alarm1);
+        alarmList.add(alarm2);
+        alarmList.add(alarm3);
+        alarmList.add(alarm4);
+        alarmList.add(alarm5);
+
+        for(AlarmInt aInt : alarmList){
+            Alist.add(aInt.getDate() + "  " + aInt.getTime());
+        }
     }
 
     public void addAlarm(String alarmDate, String alarmTime){
--- app/src/main/java/com/example/pchan/mysqldemo/SetAlarm.java
+++ app/src/main/java/com/example/pchan/mysqldemo/SetAlarm.java
@@ -6,13 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.util.Date;
 
 public class SetAlarm extends AppCompatActivity {
     TextView curTime;
+    int day, month, year, hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +24,23 @@ public class SetAlarm extends AppCompatActivity {
         curTime = (TextView)findViewById(R.id.curTime);
         String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
         curTime.setText(currentDateTimeString);
+
+
     }
 
-    public void OnBack(View view) {
+    public void OnBack (View view) {
         onBackPressed();
     }
 
     public void onSet(View view) {
-        onBackPressed();
+        DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
+        day = datePicker.getDayOfMonth();
+        month = datePicker.getMonth();
+        year = datePicker.getYear();
+        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+        hour = timePicker.getCurrentHour();
+        minute = timePicker.getCurrentMinute();
+        //return(year + "-" + month + "-" + day + "   " + hour + ":" + minute);
     }
 
 }
