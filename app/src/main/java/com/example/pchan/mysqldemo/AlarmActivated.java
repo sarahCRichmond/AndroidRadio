49af02f049237f96e3fabd32a1aa4dc35a549b29
Aftershockzap <danielfranktaylor@gmail.com>
2017-12-05 18:13:48 -0600

    Changed the alarm so that it plays a song instead of displaying a screen.

--- app/src/main/java/com/example/pchan/mysqldemo/AlarmActivated.java
+++ app/src/main/java/com/example/pchan/mysqldemo/AlarmActivated.java
@@ -10,17 +10,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Random;
 
 public class AlarmActivated extends AppCompatActivity {
+    public static final String Extra_Message = "";
+    private ArrayList<Music> arrayList;
+    private CustomMusicAdapter adapter;
+    private ListView songList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm_activated);
+        //setContentView(R.layout.activity_alarm_activated);
+
+        songList = (ListView) findViewById(R.id.songList);
+        arrayList = new ArrayList<>();
+        arrayList.add(new Music("Kalimba", "Mr. Scruff", R.raw.kalimba, "Ninja Tune", 2008, "5:48",R.drawable.kalimba));
+
+        setContentView(R.layout.activity_music_player);
+        Intent myIntent = new Intent(this, music_player.class);
+        myIntent.putExtra(Extra_Message,new String[] {arrayList.get(0).getArtist(),arrayList.get(0).getName()});
+        startActivity(myIntent);
     }
 
     public void OnBack (View view) {
